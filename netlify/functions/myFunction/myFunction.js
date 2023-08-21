5f2e5426a78ea80f620ff11bf5a66d95c8677cc6
Eyrooooon <aaron.github@carelulu.com>
2023-08-22 07:00:06 +0800

    add twilio lookup

--- netlify/functions/myFunction/myFunction.js
+++ netlify/functions/myFunction/myFunction.js
@@ -1,4 +1,9 @@
 const axios = require('axios');
+const twilio = require('twilio');
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 exports.handler = async function(event, context) {
   const endpoint = 'https://accounts.zoho.com/oauth/v2/token';
@@ -15,29 +20,35 @@ exports.handler = async function(event, context) {
     };
   }
 
-  // Get variables from environment
-  const client_id = process.env.CLIENT_ID;
-  const client_secret = process.env.CLIENT_SECRET;
-  const refresh_token = process.env.REFRESH_TOKEN;
-  const grant_type = process.env.GRANT_TYPE;
+  const phoneData = await client.lookups.v1.phoneNumbers(event.body.Phone_3)
+    .fetch({type: ['carrier', 'caller-name']});
+
+  const data = {
+    data: [
+      {
+        ...event.body,
+        Carrier: phoneData.carrier.name,
+        Number_Type: phoneData.carrier.type
+      }
+    ]
+  };
 
   try {
     const response = await axios.post(endpoint, null, {
       params: {
-        client_id,
-        client_secret,
-        refresh_token,
-        grant_type
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: process.env.REFRESH_TOKEN,
+        grant_type: process.env.GRANT_TYPE
       }
     });
 
     const accessToken = response.data.access_token;
 
-    // Use the access token to make a POST request to Zoho API
-    const zohoResponse = await axios.post('https://www.zohoapis.com/crm/v2/Leads', event.body, {
+    const zohoResponse = await axios.post('https://www.zohoapis.com/crm/v2/Leads', data, {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${accessToken}`
       }
     });
 
