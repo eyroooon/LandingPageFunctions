70f6061851f8929cf898fbc6231f78c95eed151a
Eyrooooon <aaron.github@carelulu.com>
2023-08-22 07:18:35 +0800

    add twilio lookup

--- netlify/functions/myFunction/myFunction.js
+++ netlify/functions/myFunction/myFunction.js
@@ -1,16 +1,4 @@
 const axios = require('axios');
-const twilio = require('twilio');
-
-const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-
-const CORS_HEADERS = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type",
-  "Access-Control-Allow-Methods": "POST, OPTIONS"
-};
 
 exports.handler = async function(event, context) {
   const endpoint = 'https://accounts.zoho.com/oauth/v2/token';
@@ -18,53 +6,61 @@ exports.handler = async function(event, context) {
   if (event.httpMethod === 'OPTIONS') {
     return {
       statusCode: 200,
-      headers: CORS_HEADERS,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
       body: ''  // OPTIONS request doesn't have a body
     };
   }
 
-  const phoneData = await client.lookups.v1.phoneNumbers(event.body.Phone_3)
-    .fetch({type: ['carrier', 'caller-name']});
-
-  const data = {
-    data: [
-      {
-        ...event.body,
-        Carrier: phoneData.carrier.name,
-        Number_Type: phoneData.carrier.type
-      }
-    ]
-  };
+  // Get variables from environment
+  const client_id = process.env.CLIENT_ID;
+  const client_secret = process.env.CLIENT_SECRET;
+  const refresh_token = process.env.REFRESH_TOKEN;
+  const grant_type = process.env.GRANT_TYPE;
 
   try {
     const response = await axios.post(endpoint, null, {
       params: {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        refresh_token: process.env.REFRESH_TOKEN,
-        grant_type: process.env.GRANT_TYPE
+        client_id,
+        client_secret,
+        refresh_token,
+        grant_type
       }
     });
 
     const accessToken = response.data.access_token;
 
-    const zohoResponse = await axios.post('https://www.zohoapis.com/crm/v2/Leads', data, {
+    // Use the access token to make a POST request to Zoho API
+    const zohoResponse = await axios.post('https://www.zohoapis.com/crm/v2/Leads', event.body, {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`
+        Authorization: `Bearer ${accessToken}`,
       }
     });
 
     return {
       statusCode: 200,
-      headers: CORS_HEADERS,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST"
+      },
       body: JSON.stringify(zohoResponse.data)
     };
 
   } catch (error) {
     return {
       statusCode: 422,
-      headers: CORS_HEADERS,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST"
+      },
       body: JSON.stringify(error.response.data)
     };
   }
