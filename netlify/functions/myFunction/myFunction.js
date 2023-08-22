4e8d2c463c2d19f6451ce4dda4d0fac6ed184568
Eyrooooon <aaron.github@carelulu.com>
2023-08-22 08:16:01 +0800

    add twilio lookup

--- netlify/functions/myFunction/myFunction.js
+++ netlify/functions/myFunction/myFunction.js
@@ -39,7 +39,7 @@ exports.handler = async function (event, context) {
     const accessToken = response.data.access_token;
     const { body } = event;
     const { Phone_3 } = body;
-    const phoneData = await client.lookups.v1.phoneNumbers(`${Phone_3}`).fetch({ type: ['carrier', 'caller-name'] });
+    const phoneData = await client.lookups.v1.phoneNumbers(`+14639994559`).fetch({ type: ['carrier', 'caller-name'] });
     const data = {
       ...body,
       Carrier: phoneData.carrier.name,
@@ -61,7 +61,7 @@ exports.handler = async function (event, context) {
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(Phone_3),
     };
   } catch (error) {
     return {
